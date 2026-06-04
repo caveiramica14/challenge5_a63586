954da744159101d8d8c0a430b7ee9ee192e1f095
caveiramica14 <micaeltiago2006@gmail.com>
2026-06-04 19:36:01 +0100

    5.4

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -14,13 +14,16 @@ public class GameManager : MonoBehaviour
     public TextMeshProUGUI gameOverText;
     public bool isGameActive;
     public Button restartButton;
+    public GameObject titleScreen;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public void StartGame(int difficulty)
     {
+        spawnRate /= difficulty;
         isGameActive = true;
         score = 0;
         StartCoroutine(SpawnTarget());
         UpdateScore(0);
+        titleScreen.gameObject.SetActive(false);
     }
 
     IEnumerator SpawnTarget()
